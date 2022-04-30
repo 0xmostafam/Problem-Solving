2c7a6b27f3b255765b7efe74fe00b159e7c16bd4
0xmostafam <mostafamohamed8881@gmail.com>
2022-04-30 17:54:44 +0200

    First Day: 2 Easy Problems

---
+++ 1. Two Sums.js
@@ -0,0 +1,24 @@
+/*
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+*/
+
+const twoSum = function(nums, target) {
+    const map = new Map();
+    let answer = []
+    nums.every((element,index) => {
+        if (!map.has(target - element)) {
+            map.set(element,index);
+            return true;
+        } else {
+            answer = [map.get(target - element), index];
+            return false;
+        }
+    });
+    return answer;
+};
+
+console.log(twoSum([2,7,11,15],9));
---
+++ 9. Palindrome Number.js
@@ -0,0 +1,23 @@
+/*
+Given an integer x, return true if x is palindrome integer.
+
+An integer is a palindrome when it reads the same backward as forward.
+
+For example, 121 is a palindrome while 123 is not.
+*/
+
+const isPalindrome = function(x) {
+    if (x < 0)
+        return false;
+    
+    number = x;
+    reverse = 0;
+    while(number >= 1){
+        reverse *= 10;
+        reverse += number % 10;
+        number = Math.floor(number / 10);
+    }
+    return reverse === x;
+}
+
+console.log(isPalindrome(121))
