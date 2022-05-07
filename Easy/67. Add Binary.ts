e9913374ca873df0683adde5227d62a08b862832
0xmostafam <mostafamohamed8881@gmail.com>
2022-05-07 17:44:52 +0200

    solved add binary

--- Easy/67. Add Binary.ts
+++ Easy/67. Add Binary.ts
@@ -1,3 +1,25 @@
 // Given two binary strings a and b, return their sum as a binary string.
 
-const addBinary = (a: string, b: string): string => {};
+const addBinary = (a: string, b: string): string => {
+  let newBinary = "";
+  let i = a.length - 1;
+  let j = b.length - 1;
+  let carry = 0;
+
+  while (i >= 0 || j >= 0) {
+    let charA = i >= 0 ? parseInt(a[i]) : 0;
+    let charB = j >= 0 ? parseInt(b[j]) : 0;
+    let sum = charA + charB + carry;
+    if (sum < 2) carry = 0;
+    else carry = 1;
+
+    newBinary += sum % 2;
+
+    i--;
+    j--;
+  }
+  if (carry === 1) newBinary += "1";
+  return [...newBinary].reverse().join("");
+};
+
+addBinary("11", "1");
